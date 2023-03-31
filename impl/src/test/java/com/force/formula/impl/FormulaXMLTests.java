8dcf4977883871a92776557db4373869fff33312
Sahil Bansal <sahilbansal@salesforce.com>
2023-03-31 15:57:12 -0700

    New Formula XML tests class for running all XML tests

---
+++ impl/src/test/java/com/force/formula/impl/FormulaXMLTests.java
@@ -0,0 +1,47 @@
+package com.force.formula.impl;
+
+import com.force.formula.DbTester;
+import com.force.formula.FormulaEngine;
+import com.force.formula.sql.EmbeddedPostgresTester;
+import com.force.formula.v2.FormulaXMLTestSuite;
+import com.force.formula.v2.IFormulaTestCaseFilter;
+import com.force.formula.v2.IFormulaTestDefinitionParser;
+import com.force.formula.v2.data.FormulaTestDefinition;
+import com.force.formula.v2.impl.FormulaTestDefinitionFileParser;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.AllTests;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+@RunWith(AllTests.class)
+public class FormulaXMLTests extends FormulaXMLTestSuite {
+
+    public FormulaXMLTests(List<String> testDefinitionAbsoluteFilePaths, IFormulaTestDefinitionParser fileParser, IFormulaTestCaseFilter testCaseFilter, String goldFileDirectory) {
+        super("FormulaStandardTests", testDefinitionAbsoluteFilePaths, fileParser, testCaseFilter, goldFileDirectory);
+    }
+
+    /**
+     * As all the tests under this suite should run with same configurations, having a beforeTest() and afterTest() at
+     * suite level makes sense.
+     * @return
+     */
+    public static TestSuite suite() {
+        List<String> xmlFiles = new ArrayList<>();
+        xmlFiles.add("src/test/resources/com/force/formula/impl/formulaTestV2.xml");
+        FormulaEngine.setHooks(new BaseCustomizableParserTest.FieldTestFormulaValidationHooks());
+        FormulaEngine.setFactory(BaseFieldReferenceTest.TEST_FACTORY);
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        IFormulaTestDefinitionParser<FormulaTestDefinition> parser = new FormulaTestDefinitionFileParser();
+        String goldFileDirectory = "src/test/goldfiles/FormulaFields/v2/postgres";
+        return new FormulaXMLTests(xmlFiles, parser, null, goldFileDirectory);
+    }
+
+    @Override
+    protected DbTester constructDbTester() throws IOException {
+        return new EmbeddedPostgresTester();
+    }
+}
